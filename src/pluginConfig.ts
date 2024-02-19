7ea4f070ee73e5ac9d9bc89942c5ef7dea1fbda1
Jochen Jacobs <JochenJacobs33@gmail.com>
2024-02-19 02:30:09 +0100

    bump version

--- src/pluginConfig.ts
+++ src/pluginConfig.ts
@@ -2,7 +2,7 @@ import type { ExternalPluginConfig } from '@windy/interfaces';
 
 const config: ExternalPluginConfig = {
     name: 'windy-plugin-sun-position',
-    version: '2.0.0-alpha.6',
+    version: '2.0.0-alpha.7',
     icon: '☼',
     title: 'Sun Position',
     description:
